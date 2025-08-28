2a52de0c5c63f85812975d2f2f06ac3172d46f1c
Adriano Ruseler <adrianoruseler@gmail.com>
2025-08-28 12:15:11 -0300

    Add Copyright

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -263,7 +263,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} STM32 Docs for ELT73A, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,

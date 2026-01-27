33cb409c5eaf9ce1ad7c7a39c685ebd06e653c34
Adriano Ruseler <adrianoruseler@gmail.com>
2026-01-27 20:35:46 -0300

    Update project's social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -189,7 +189,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/ELT73A-BannerCourse.png",
     navbar: {
       title: "ELT73A",
       logo: {

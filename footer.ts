0db28f36c3f0ffecc13a4778df88a2a47f2145ae
Adriano Ruseler <adrianoruseler@gmail.com>
2026-02-27 08:39:32 -0300

    Update footer.ts

--- footer.ts
+++ footer.ts
@@ -43,6 +43,121 @@ const footer: ThemeConfig["footer"] = {
             ],
         },
         {
+            title: "UTFPR",
+            items: [
+                {
+                    label: "Moodle",
+                    href: "https://moodle.utfpr.edu.br/",
+                },
+                {
+                    label: "Webmail",
+                    href: "https://webmail.utfpr.edu.br/",
+                },
+                {
+                    label: "Sistemas",
+                    href: "https://sistemas2.utfpr.edu.br/",
+                },
+                {
+                    label: "SEI",
+                    href: "https://sei.utfpr.edu.br/",
+                },
+                {
+                    label: "Chat",
+                    href: "https://chat.utfpr.edu.br/",
+                },
+                {
+                    label: "Ajuda",
+                    href: "https://ajuda.utfpr.edu.br/",
+                },
+                {
+                    label: "Calendário",
+                    href: "https://www.utfpr.edu.br/alunos/calendario",
+                },
+                {
+                    label: "TCC",
+                    href: "https://nupet.daelt.ct.utfpr.edu.br/tcc/engenharia/index.html",
+                },
+                {
+                    label: "Nuvem",
+                    href: "https://nuvem.utfpr.edu.br/",
+                },
+            ],
+        },
+        {
+            title: "Sistemas",
+            items: [
+                {
+                    label: "Sigepe",
+                    href: "https://sougov.sigepe.gov.br/sougov/Home",
+                },
+                {
+                    label: "gov.br",
+                    href: "https://servicos.acesso.gov.br/",
+                },
+                {
+                    label: "Consulta Diários",
+                    href: "https://sistemas2.utfpr.edu.br/url/1141",
+                },
+                {
+                    label: "Permanências",
+                    href: "https://sistemas2.utfpr.edu.br/url/1026",
+                },
+                {
+                    label: "Planejamento",
+                    href: "https://sistemas2.utfpr.edu.br/url/1890",
+                },
+                {
+                    label: "Lançamentos",
+                    href: "https://sistemas2.utfpr.edu.br/url/1514",
+                },
+                {
+                    label: "Mapa de Professor",
+                    href: "https://sistemas2.utfpr.edu.br/url/1022",
+                },
+                {
+                    label: "Mapa de Sala",
+                    href: "https://sistemas2.utfpr.edu.br/url/1934",
+                },
+            ],
+        },
+        {
+            title: "AI", //
+            items: [
+                {
+                    label: "Gemini",
+                    href: "https://gemini.google.com/app",
+                }, //
+                {
+                    label: "ChatGPT",
+                    href: "https://chatgpt.com/",
+                }, //
+                {
+                    label: "Claude",
+                    href: "https://claude.ai/",
+                },
+                {
+                    label: "Copilot",
+                    href: "https://copilot.microsoft.com/",
+                },
+                {
+                    label: "DeepSeek",
+                    href: "https://chat.deepseek.com/",
+                },
+                {
+                    label: "Grok",
+                    href: "https://grok.com/",
+                },
+                {
+                    label: "Kimi",
+                    href: "https://www.kimi.com/en",
+                },
+                {
+                    label: "GLM",
+                    href: "https://chat.z.ai/",
+                },
+            ],
+        },
+        {
             title: "Recursos",
             items: [
                 {

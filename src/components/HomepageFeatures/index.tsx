e06dcc0e65afd4b944bce8986984ae9fa4b5acc2
Adriano Ruseler <ruseler@utfpr.edu.br>
2026-08-24 09:25:53 -0300

    Refactor HomepageFeatures to use SVG icons
    Convert HomepageFeatures to use inline SVG components and Docusaurus Link: change FeatureItem.title to ReactNode, require SVGs from @site/static, remove raster image imports, and simplify rendering (always render Svg). Adjust layout from col--3 to col--4 and update feature entries/descriptions to use site routes. Add four new SVG assets: static/img/factory-svgrepo-com.svg, industrial-robot-robot-svgrepo-com.svg, iot-platform-svgrepo-com.svg, microchip-svgrepo-com.svg. Also remove unused imports and commented-out legacy feature.

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,117 +1,86 @@
-import type { ReactNode } from "react";
-import clsx from "clsx";
-import Heading from "@theme/Heading";
-import styles from "./styles.module.css";
-
-//import useBaseUrl from "@docusaurus/useBaseUrl";
-import GitHubImage from "/img/github-mark.png"; // Use @site alias for src folder
-import VSCodeImage from "/img/vscode.png";
-import STM32Image from "/img/STM32.png";
-import PIOimage from "/img/platformio.png";
+import type { ReactNode } from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+import Link from "@docusaurus/Link";
 
 type FeatureItem = {
-  title: string;
-  Svg?: React.ComponentType<React.ComponentProps<"svg">>;
-  image?: string;
+  title: React.ReactNode; // Changed to ReactNode to allow links
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
+  /* {
+     title: (
+       <Link to="/utfpr/ELT72B/intro">
+         Sistemas Digitais
+       </Link>
+     ),
+     Svg: require('@site/static/img/office-svgrepo-com.svg').default,
+     description: (
+       <>
+         A disciplina de sistemas digitais é gerenciada via {" "}
+         <a href="https://moodle.utfpr.edu.br/course/view.php?id=27864" target="_blank">
+           Moodle
+         </a>, o <Link to="/ead/intro">EaD</Link> com videoaulas e questionários. O conteúdo é disponibilizado no{" "}<a href="https://moodle.utfpr.edu.br/course/view.php?id=27864" target="_blank">
+           Moodle
+         </a>{" "}e as atividades são realizadas de forma assíncrona, permitindo que os alunos possam acessar o conteúdo e realizar as atividades no seu próprio ritmo.
+ 
+         Ministrada via{" "}
+         <a href="https://moodle.utfpr.edu.br/course/view.php?id=27864" target="_blank">
+           Moodle
+         </a>{" "}!
+       </>
+     ),
+   }, */
   {
-    title: "GitHub Repository",
-    // Svg: require("@site/static/img/github-mark.svg").default,
-    image: GitHubImage, // Use the imported image variable here
+    title: (
+      <Link to="/quiz/intro">
+        Ensino a Distância
+      </Link>
+    ),
+    Svg: require('@site/static/img/microchip-svgrepo-com.svg').default,
     description: (
       <>
-        O{" "}
-        <a href="https://github.com/" target="_blank">
-          GitHub
-        </a>{" "}
-        é uma plataforma de desenvolvimento de software baseada em nuvem, criada
-        para hospedar e gerenciar projetos de código usando o sistema de
-        controle de versão
-        <a
-          href="https://training.github.com/downloads/pt_BR/github-git-cheat-sheet/"
-          target="_blank"
-        >
-          {" "}
-          Git
-        </a>
-        .
+        As atividades de <Link to="/quiz/intro">ensino a distância</Link> constituem em uma sequencia de videoaulas explicativas acompanhadas de questionários. As atividades são realizadas de forma assíncrona, permitindo que os alunos possam acessar o conteúdo e realizar as atividades no seu próprio ritmo.
       </>
     ),
   },
   {
-    title: "Visual Studio Code",
-    // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-    image: VSCodeImage, // Use the imported image variable here
+    title: (
+      <Link to="/labs/intro">
+        Laboratório
+      </Link>
+    ),
+    Svg: require('@site/static/img/factory-svgrepo-com.svg').default,
     description: (
       <>
-        <a href="https://code.visualstudio.com/" target="_blank">
-          Visual Studio Code
-        </a>{" "}
-        é uma ferramenta leve, rápida e extremamente flexível, que se adapta ao
-        fluxo de trabalho de qualquer desenvolvedor, de iniciantes a
-        profissionais experientes.
+        As atividades de <Link to="/labs/intro">laboratório</Link> constituem da aplicação prática dos conteúdos vistos no <Link to="/quiz/intro">ensino a distância</Link>. As atividades são realizadas de forma síncrona, com encontros semanais para a realização das atividades práticas, promovendo a interação entre os alunos e a aplicação dos conceitos vistos.
       </>
     ),
   },
   {
-    title: "PlatformIO IDE",
-    //Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-    image: PIOimage, // Use the imported image variable here
+    title: (
+      <Link to="/projects/intro">
+        Projeto Final
+      </Link>
+    ),
+    Svg: require('@site/static/img/industrial-robot-robot-svgrepo-com.svg').default,
     description: (
       <>
-        O{" "}
-        <a
-          href="https://platformio.org/"
-          target="_blank"
-        >
-          PlatformIO
-        </a>{" "}
-        é um ecossistema de desenvolvimento multiplataforma e de arquitetura cruzada projetado para engenheiros de sistemas embarcados e desenvolvedores que criam aplicações para produtos embarcados, especialmente na área de Internet das Coisas (IoT).
+        O <Link to="/projects/intro">projeto final</Link> constitue em aplicar os conhecimentos adquiridos ao longo do curso para desenvolver um protótipo utilizando o <Link to="/docs/black-pill">WeAct BlackPill</Link>. O projeto é desenvolvido em grupos, promovendo a colaboração entre os alunos e a aplicação prática dos conceitos aprendidos.
       </>
     ),
   },
-  {
-    title: "Arm® Cortex® M4 32-bit RISC Core",
-    //Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-    image: STM32Image, // Use the imported image variable here
-    description: (
-      <>
-        A placa{" "}
-        <a
-          href="https://github.com/WeActStudio/WeActStudio.MiniSTM32F4x1"
-          target="_blank"
-        >
-          Black Pill
-        </a>{" "}
-        com o{" "}
-        <a
-          href="https://www.st.com/en/microcontrollers-microprocessors/stm32f411.html"
-          target="_blank"
-        >
-          STM32F411CEU6
-        </a>{" "}
-        é uma opção de excelente custo-benefício para quem precisa de mais poder
-        de computação e flexibilidade do que plataformas como o{" "}
-        <a href="https://www.arduino.cc/" target="_blank">
-          Arduino
-        </a>{" "}
-        podem oferecer.
-      </>
-    ),
-  },
+
 ];
-function Feature({ title, Svg, image, description }: FeatureItem) {
+
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx("col col--3")}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
-        {Svg ? (
-          <Svg className={styles.featureSvg} role="img" />
-        ) : image ? (
-          <img src={image} className={styles.featureImage} alt={title} />
-        ) : null}
+        <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
